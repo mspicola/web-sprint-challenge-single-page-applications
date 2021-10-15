5d63ea7722f801e13323b9baa88ebc0d334ce818
mspicola <mspicola@protonmail.com>
2021-10-15 18:22:32 -0400

    created components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import './App.css'
+import PizzaHeader from "./PizzaHeader";
+import {Route, Link, Switch } from 'react-router-dom';
+import PizzaForm from "./PizzaForm";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="head">
+    <PizzaHeader/>
+      <Switch>
+         <Route path='/Pizza' component={PizzaForm} />
+    
+    
+    
+      </Switch>
+    </div>
   );
 };
 export default App;
